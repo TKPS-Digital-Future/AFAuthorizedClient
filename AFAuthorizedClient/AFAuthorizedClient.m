8e3039ae9e58d3a7c58fc46e30c05da6346da9ca
Patric Schenke <booiiing@gmail.com>
2013-06-14 12:50:18 +0200

    Failure-block to check for required authorization and get it from the delegate

--- AFAuthorizedClient/AFAuthorizedClient.m
+++ AFAuthorizedClient/AFAuthorizedClient.m
@@ -7,7 +7,71 @@
 //
 
 #import "AFAuthorizedClient.h"
+#import "AFURLConnectionOperation.h"
 
 @implementation AFAuthorizedClient
 
+- (AFHTTPRequestOperation *)HTTPRequestOperationWithRequest:(NSURLRequest *)urlRequest
+                                                    success:(void (^)(AFHTTPRequestOperation *, id))success
+                                                    failure:(void (^)(AFHTTPRequestOperation *, NSError *))failure
+{
+    void (^authorizationFailure)(AFHTTPRequestOperation *, NSError *) = ^(AFHTTPRequestOperation *request, NSError *error)
+    {
+        BOOL investigateHTTP = NO;
+        BOOL authorizationFailed = NO;
+        
+        NSInteger errorCode = error.code;
+        
+        switch (errorCode)
+        {
+            case NSURLErrorUserAuthenticationRequired:
+            {
+                authorizationFailed = YES;
+            } break;
+                
+            case NSURLErrorUnknown:
+            default:
+            {
+                investigateHTTP = YES;
+            } break;
+        }
+        
+        if (investigateHTTP)
+        {
+            NSDictionary *userInfo = error.userInfo;
+            NSHTTPURLResponse *httpResponse = userInfo[AFNetworkingOperationFailingURLResponseErrorKey];
+            NSInteger statusCode = httpResponse.statusCode;
+            
+            switch (statusCode)
+            {
+                case 401:
+                {
+                    authorizationFailed = YES;
+                } break;
+                    
+                default:
+                    break;
+            }
+        }
+        
+        if (authorizationFailed)
+        {
+            NSMutableURLRequest *mutableRequest = urlRequest.mutableCopy;
+            
+            NSMutableDictionary *headers = mutableRequest.allHTTPHeaderFields.mutableCopy;
+            [headers setValue:self.authorizationDelegate.authorizationHeader forKey:@"Authorization"];
+            
+            mutableRequest.allHTTPHeaderFields = headers;
+            
+            AFHTTPRequestOperation *retryOperation = [self HTTPRequestOperationWithRequest:mutableRequest success:success failure:failure];
+            [self enqueueHTTPRequestOperation:retryOperation];
+        }
+        else
+        {
+            failure(request, error);
+        }
+    };
+    
+    return [super HTTPRequestOperationWithRequest:urlRequest success:success failure:authorizationFailure];
+}
 @end
