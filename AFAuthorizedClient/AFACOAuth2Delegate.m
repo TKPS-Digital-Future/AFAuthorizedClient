af99f5581b4a76928c590c45268e9e725cdc2e17
Patric Schenke <booiiing@gmail.com>
2013-06-14 16:21:21 +0200

    Inline-documentation for token-request-chain

--- AFAuthorizedClient/AFACOAuth2Delegate.m
+++ AFAuthorizedClient/AFACOAuth2Delegate.m
@@ -68,20 +68,25 @@ NSInteger const kNoAuthcodeRedirectURIError = 3;
 
 - (void) authenticateWithSuccess:(void (^)(AFOAuthCredential *credential))success failure:(void (^)(NSError *error))failure
 {
+    // Type-definition for readable code
     typedef void (^failureBlock)(NSError *);
     
+    // Blocks used to request a token with various methods
     void (^refreshTokenRequest)(failureBlock);
     void (^usernamePasswordRequest)(failureBlock);
     void (^authCodeRequest)(failureBlock);
     void (^implicitRequest)(failureBlock);
     
+    // Request a token using a refresh
     refreshTokenRequest = ^(failureBlock refreshFailure)
     {
+        // Find a refresh token, either set with the setCredentials-method or provided by the existing credential
         NSString *refreshToken = self.refreshToken?self.refreshToken:((self.afOAuthCredential && self.afOAuthCredential.refreshToken)?self.afOAuthCredential.refreshToken:nil);
         
         if (refreshToken) {
             [self.oauth2Client authenticateUsingOAuthWithPath:self.tokenEndpoint refreshToken:refreshToken success:success failure:refreshFailure];
         }
+        // If no refresh-token is present, the failure-block gets called with a specific error-object
         else
         {
             NSError *noRefreshTokenError = [NSError errorWithDomain:kAFAuthorizedClientErrorDomain code:kNoRefreshTokenError userInfo:nil];
@@ -89,11 +94,14 @@ NSInteger const kNoAuthcodeRedirectURIError = 3;
         }
     };
     
+    // Request a token using username-password-authentication
     usernamePasswordRequest = ^(failureBlock upFailure)
     {
+        // Check for username and password set by the setCredentials-method
         if (self.username && self.password) {
             [self.oauth2Client authenticateUsingOAuthWithPath:self.tokenEndpoint username:self.username password:self.password scope:self.scope success:success failure:upFailure];
         }
+        // If either username or password are not set, a specific error is handed to the failure-block
         else
         {
             NSError *noUsernamePasswordError = [NSError errorWithDomain:kAFAuthorizedClientErrorDomain code:kNoUsernamePasswordError userInfo:nil];
@@ -101,11 +109,13 @@ NSInteger const kNoAuthcodeRedirectURIError = 3;
         }
     };
     
+    // Request a token using auth-code and redirect-URI
     authCodeRequest = ^(failureBlock acFailure)
     {
         if (self.authCode && self.redirectURI) {
             [self.oauth2Client authenticateUsingOAuthWithPath:self.tokenEndpoint code:self.authCode redirectURI:self.redirectURI success:success failure:acFailure];
         }
+        // If either auth-code or redirect-URI are not set, a specific error is handed to the failure-block
         else
         {
             NSError *noAuthcodeRedirectURIError = [NSError errorWithDomain:kAFAuthorizedClientErrorDomain code:kNoAuthcodeRedirectURIError userInfo:nil];
@@ -113,26 +123,32 @@ NSInteger const kNoAuthcodeRedirectURIError = 3;
         }
     };
     
+    // Request a token using implicit authentication
     implicitRequest = ^(failureBlock impFailure)
     {
         [self.oauth2Client authenticateUsingOAuthWithPath:self.tokenEndpoint scope:self.scope success:success failure:impFailure];
     };
     
+    // If auth-code-request fails, try implicit request
     failureBlock acFailure = ^(NSError *error)
     {
+        // If implicit request fails, fail completely
         implicitRequest(failure);
     };
     
+    // If username-password fails, try auth-code
     failureBlock upFailure = ^(NSError *error)
     {
         authCodeRequest(acFailure);
     };
     
+    // If refresh-token fails, try username-password
     failureBlock refreshFailure = ^(NSError *error)
     {
         usernamePasswordRequest(upFailure);
     };
     
+    // Try refresh-request
     refreshTokenRequest(refreshFailure);
 }
 
